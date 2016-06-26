5fecea37a653e3e4f003445cc0b792fe5a59b450
Erik Quinn <erikquinn2@gmail.com>
2016-06-26 16:11:42 -0500

    Add sourcemaps & minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,22 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
 
 gulp.task('es6', () => {
 	return gulp.src('src/app.js')
+		.pipe(sourcemaps.init())
 		.pipe(babel({
 			presets: ['es2015']
 		}))
+		.pipe(uglify())
+		.pipe(rename('app.min.js'))
+		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest('build'));
 });
 
 gulp.task('default', () => {
-	gulp.watch('src/app.js', ['es6'])
+	gulp.start('es6');
+	gulp.watch('src/*.*', ['es6'])
 });
